acea30ce3b5ed0777c60b71f46e877c7fa4fe98b
Miguel Henriquez <henriquez.miguel@outlook.com>
2019-02-12 21:08:52 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,39 +1,57 @@
-def dictionary
-  dictionary = {
-    "hello" => 'hi',
-    "to" => '2',
-    "two" => '2',
-    'too' => '2',
-    "four" => '4',
-    'be' => 'b',
-    'you' => 'u',
-    "at" => "@" ,
-    "and" => "&",
-  }
-end
+phrase = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
 
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-array.each do |name|
-  name.split('').first
+# Write your code here.
+require "pry"
+
+def dictionary
+  convert = {
+   "hello" => "hi",
+   "to" => "2",
+   "two" => "2",
+   "too" => "2",
+   "for" => "4",
+   "four" => "4",
+   "be" => "b",
+   "you" => "u",
+   "at" => "@",
+   "and" => "&"
+}
 end
 
 def word_substituter(tweet)
-  new_array = []
-  match = false
-  tweet.split(' ').each do |word|
-    if match == false
-      dictionary.each do |key, value|
-        if word == key
-          new_array << value
-          match = true
-        end
-      end
+  # replaces long words with their expected short form
+
+    tweet.split(" ").map do |word|
+      if dictionary.keys.include?(word.downcase)
+        word = dictionary[word.downcase]
+      else
+        word
     end
-    if match == false
-      new_array << word 
-    end
-  end
-  new_array
+  end.join(" ")
 end
 
-word_substituter("hello butt head and hello to you")
+def bulk_tweet_shortener(tweet)
+  tweet.map do |phrase|
+    puts word_substituter(phrase)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  tweet.split(" ").map do |phrase|
+    if phrase.length > 140
+      word_substituter(phrase)
+    elsif phrase.length <= 140
+      phrase
+    end
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  tweet.split(" ").map do |phrase|
+    if phrase.length > 140
+      word_substituter(phrase)[0..140] + "..."
+    else
+      phrase
+    end
+  end.join(" ")
+end
